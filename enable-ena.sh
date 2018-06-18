0e55e36ca0a3b44ebeec6a9cb337754695099ea8
Timothy Neilen <tneilen@gmail.com>
2018-06-18 13:15:35 +1000

    couple of inclusions according to best practice

--- enable-ena.sh
+++ enable-ena.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefailx
+
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@" ]
 
 apt-get updates
 apt-get install -y build-essential dkms
-$version=`wget -qO- --no-check-certificate https://github.com/amzn/amzn-drivers/releases | sed -n 's|.*/archive/ena_linux_\(.*\).tar.gz.*|\1|p' | awk '{ print $1; exit }'`
+$VERSION=`wget -qO- --no-check-certificate https://github.com/amzn/amzn-drivers/releases | sed -n 's|.*/archive/ena_linux_\(.*\).tar.gz.*|\1|p' | awk '{ print $1; exit }'`
 git clone https://github.com/amzn/amzn-drivers /usr/src
-mv /usr/src/amzn-drivers /usr/src/amzn-drivers-${version}
-cat > /usr/src/amzn-drivers-${version}/dkms-test1.conf << EOF
+mv /usr/src/amzn-drivers /usr/src/amzn-drivers-${VERSION}
+cat > /usr/src/amzn-drivers-${VERSION}/dkms-test1.conf << EOF
 PACKAGE_NAME="ena"
-PACKAGE_VERSION="${version}"
+PACKAGE_VERSION="{VERSION}"
 CLEAN="make -C kernel/linux/ena clean"
 MAKE="make -C kernel/linux/ena/ BUILD_KERNEL=\${kernelver}"
 BUILT_MODULE_NAME[0]="ena"
@@ -18,7 +21,7 @@ DEST_MODULE_NAME[0]="ena"
 AUTOINSTALL="yes"
 EOF
 
-dkms add -m amzn-drivers -v ${version}
-dkms build -m amzn-drivers -v ${version}
-dkms install -m amzn-drivers -v ${version}
+dkms add -m amzn-drivers -v ${VERSION}
+dkms build -m amzn-drivers -v ${VERSION}
+dkms install -m amzn-drivers -v ${VERSION}
 update-initramfs -c -k all
